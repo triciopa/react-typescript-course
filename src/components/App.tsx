28bd885403fab368df2251d8a3c3e54fe3af467b
Patricio <patriciogatti@gmail.com>
2021-07-25 19:46:06 -0300

    Auth service

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,19 @@
-function App() {
-  return (
-    <div className="App">App works
-    </div>
-  );
-}
+import React from "react";
+import { User } from "../model/Model";
+import { AuthService } from "../services/AuthService";
 
-export default App;
+
+
+interface AppState{
+  user: User | undefined
+}
+export class App extends React.Component<{}, AppState> {
+
+  private authService : AuthService = new AuthService();
+  
+  render(){
+    return (
+      <div>App works</div>
+    )
+  }
+}
